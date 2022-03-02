544651fa5d7e9d21a5b6a6b162eed49a5fa51048
reivilo3 <oliviero3@hotmail.com>
2022-03-02 17:32:48 +0100

    TEST01 IMAGES

---
+++ capture_png.py
@@ -0,0 +1,28 @@
+from time import sleep
+import picamera
+import picamera.array
+import numpy as np
+from PIL import Image
+
+if __name__== '__main__':
+    camera = picamera.PiCamera()
+    camera.color_effects = (128, 128) # The reason why grey image
+    # camera.resolution = camera.MAX_RESOLUTION  #To save in PNG format, I have to comment this.
+    # Also,if I dont define the resolution, it will be in max resolution, who is 1920,1080
+    camera.start_preview(resolution=(410,313),fullscreen=False,window=(20,20,820,616))
+    camera.exposure_mode = 'auto'
+    
+    sleep(2)
+    camera.capture('/home/pi/Documents/diffusercam/saved_images/test01/t2d55.png', 'png')
+    camera.stop_preview()
+
+    # for i in range(1):
+    #     stream = picamera.array.PiBayerArray(camera)
+    #     camera.capture(stream, 'png')
+    #     # camera.capture(stream, 'jpeg', bayer=True)
+    #     #I added this next 2 lines to get a photo after 3 seconds
+    #     # sleep(3)
+    #     # camera.capture('/home/pi/Documents/diffusercam/saved_images/test01/t1d0.png')
+
+    #     input("Press Enter to stop")
+
--- preview_grey.py
+++ preview_grey.py
@@ -8,15 +8,15 @@ if __name__== '__main__':
     camera = picamera.PiCamera()
     camera.color_effects = (128, 128) # The reason why grey image
     camera.resolution = camera.MAX_RESOLUTION
-    camera.start_preview(resolution=(410,313),fullscreen=False,window=(20,20,820,616))
+    camera.start_preview(resolution=(410,313),fullscreen=True)#,window=(20,20,820,616))
     camera.exposure_mode = 'auto'
             
     for i in range(1):
         stream = picamera.array.PiBayerArray(camera)
         camera.capture(stream, 'jpeg', bayer=True)
-        #I added this next 2 lines to get a photo after 5 seconds
-        sleep(5)
-        camera.capture('/home/pi/Documents/diffusercam/saved_images/h_0001.jpg')
+        #I added this next 2 lines to get a photo after 3 seconds
+        # sleep(3)
+        # camera.capture('/home/pi/Documents/diffusercam/saved_images/test01/t1d0.jpg')
 
         input("Press Enter to stop")
 
